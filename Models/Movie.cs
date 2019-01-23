67767af866d4c965ff99b5d69b125300206c4614
lubelle <belleliullh@gmail.com>
2019-01-23 12:00:02 -0500

    add new/update existing customer form

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -9,17 +9,23 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         [Required]
         public DateTime ReleaseDate { get; set; }
+
         [Required]
         public DateTime DateAdded { get; set; }
+
         [Required]
         public byte NumberInStock { get; set; }
+
         [Required]
         public Genre Genre { get; set; }    // Navigation Property
+
         public byte GenreId { get; set; }   // foreign key
     }
 }
